4146cb9f7e6086e2d46d46b9335e02fe2c848288
basit2023 <babaralibj362@gmail.com>
2024-10-23 13:09:41 +0500

    The practice Code updated for Day3 week2

---
+++ Week2/Day3/Day3.js
@@ -0,0 +1,64 @@
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+// let car1="car1"
+//very dificult to find some specific ones so the fixed by array
+
+//up to 300
+ const arr=[] //represent
+//  const arr=new Array() // we can also initilized through this as well like the above
+//  const arr1=new Array("car1","car2",3,undefined,null);
+//  console.log("the array 1 is:",arr1)
+
+// const arr2=[1,2,3,4,5,6]
+// console.log("the arr2 is:",arr2)
+// arr2[0]="car";    // updated the index value. 
+
+// console.log(arr2)
+// arr2[10]="asaad" //update opereation, the empty index will give us the undefined value
+// console.log("the array dynamic size",arr2)
+// console.log("the array length",arr2.length)
+//find the index position
+// console.log(arr2[6])
+//push add element at the end of an array
+
+const num1=["car",30, "name","apple"]
+console.log("the original array is:",num1)
+// num1.push("Banana")
+// console.log("the updated array is",num1)
+//pop() to remove element from the end of array
+// num1.pop()
+// console.log("Remove Elemnet from the end of an array:",num1)
+
+//unshift() used to add element at the atart of an array
+// num1.unshift("Banana")
+// console.log("the updated array after unshift:", num1)
+
+//shift to remove elemet from the start of an array
+// num1.shift()
+// console.log("updated array after removing element from the start:",num1)
+
+// const num2=["this","is", "me"]
+// console.log("the array 2 is:",num2)
+// const num3=num1.concat(num2)
+// console.log("the result after adding two array:",num3)
+
+const sort1=[9,8,5,6,7,4,2,3,1]
+// console.log(sort1.sort())  // to sort numbers
+// console.log("reverse array:",sort1.reverse()) //to revers complete array
+for(let i=0; i<num1.length; i++){
+    console.log(`The index:${i} value is:${num1[i]}`)
+}
+
+
+
