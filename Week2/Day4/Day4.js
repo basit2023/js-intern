997eee0010779a066b432ebc62aecdce527ec736
basit2023 <babaralibj362@gmail.com>
2024-10-24 15:50:44 +0500

    day4 week 2 code added

--- Week2/Day4/Day4.js
+++ Week2/Day4/Day4.js
@@ -56,14 +56,14 @@ const a=[1,2,3,4,5,6,8,9,10];
 //  console.log(fore1)
 
 
-// slice
+// slice    the slice method doesnot distub the original array wh
 const fruits = ['apple', 'banana', 'mango', 'orange']
 const result=fruits.slice(0,3)
 console.log("the original array:",fruits)
 console.log("the new array:",result)
 
 const fruits1 = ['apple', 'banana', 'mango', 'orange']
-
+//the splice method disturb the original array and give us a shallow copy of the original array.
 const result1=fruits1.splice(0,1,"kochbe")
 console.log("the originale array of fruits1 is:",fruits1)
 console.log("the updated array of fruits1 is:",result1)
